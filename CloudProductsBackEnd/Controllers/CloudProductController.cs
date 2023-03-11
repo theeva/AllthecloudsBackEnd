85f4722ef14278d98512d7ade135f71b87a96261
theevagar <theevagar@ogglobal.net>
2023-03-12 01:58:07 +0800

    Add project files.

---
+++ CloudProductsBackEnd/Controllers/CloudProductController.cs
@@ -0,0 +1,73 @@
+﻿using CloudProductsBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.PortableExecutable;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CloudProductsBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CloudProductController : ControllerBase
+    {
+
+        private readonly ILogger<CloudProductController> _logger;
+
+        private  IConfiguration _configuration;
+
+        public CloudProductController(ILogger<CloudProductController> logger, IConfiguration iConfig)
+        {
+            _logger = logger;
+            _configuration = iConfig;
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            string dbConn = _configuration.GetSection("MySettings").GetSection("ApiUrl").Value;
+
+            double markupvalue = Convert.ToDouble(_configuration.GetSection("MySettings").GetSection("markupvalue").Value);
+            string ApiUrl = _configuration.GetSection("MySettings").GetSection("ApiUrl").Value;
+            string Header = _configuration.GetSection("MySettings").GetSection("Header").Value; 
+
+            var products =  GetJsonAsync<List<Product>>(ApiUrl, Header).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            products.ForEach(p =>
+            {
+                p.markupPrice = p.unitPrice * markupvalue;
+                p.maximumQuantity = p.maximumQuantity == null ? 0 : p.maximumQuantity;
+            });
+
+            return products;
+        }
+
+        public async Task<T> GetJsonAsync<T>(string url, string Header)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                if (!string.IsNullOrEmpty(Header))
+                {
+                    httpClient.DefaultRequestHeaders.Add("api-key", Header);
+                }
+
+                var response = await httpClient.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.StatusCode.ToString());
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<T>(content);
+                return result;
+            }
+        }
+    }
+}
---
+++ CloudProductsBackEnd/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudProductsBackEnd.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public double unitPrice { get; set; }
+        public double markupPrice { get; set; }
+        public string productId { get; set; }
+        public string description { get; set; }
+        public int? maximumQuantity { get; set; }
+    }
+}
